96f78e058897dcaa6d453ed2dc26e5a7c571493a
Naoto Kawahara <nkawahara@comp.is.uec.ac.jp>
2016-04-18 02:29:10 +0900

    more get details from requests/status.xml

--- vlc_nowplaying.rb
+++ vlc_nowplaying.rb
@@ -20,24 +20,37 @@ Plugin.create(:vlc_nowplaying) do
           visible: true,
           role: :timeline) do |hoge|
     # start point
-    uri = "#{UserConfig[:host]}#{(UserConfig[:host]=~/\/$/)?'':'/'}requests/playlist.xml"
+    uri = "#{UserConfig[:host]}#{(UserConfig[:host]=~/\/$/)?'':'/'}requests/status.xml"
+    raw_data = ""
+    post_data = ""
     title = ""
+    artist = ""
+    album = ""
     begin
       f = open(uri, {:http_basic_authentication => ['', UserConfig[:password]]})
       raw = f.read
       doc = REXML::Document.new(raw)
-      doc.elements.each("node/node/leaf[@current='current']") do |elem|
-        title = elem.attributes["name"]
-      end
+      raw_data = doc.elements["//root/information"].to_s 
+      begin
+        title = doc.elements["//root/information/category/info[@name='title']"].text
+        artist = doc.elements["//root/information/category/info[@name='artist']"].text
+        album = doc.elements["//root/information/category/info[@name='album']"].text
+        post_data = title + ", " + album + ", " + artist + ", #now_playing #mikutter "
+      rescue => exp
+        if title == "" then
+          post_data=doc.elements["//root/information/category/info[@name='filename']"].text
+		    end
+	    end
+      #Plugin.call(:update, nil, [Message.new(:message => raw_data, :system => true)])  
+      #Plugin.call(:update, nil, [Message.new(:message => post_data, :system => true)])  
     rescue => exp
+      Plugin.call(:update, nil, [Message.new(:message => raw_data, :system => true)])  
       logger "VLC nowplaying error: \"#{exp.message}\" -- Please check vlc status or plugin settings"
     end
-      #p title
-    if title != "" then
-      Service.primary.update(:message => "#{title} #nowplaying")
+    if post_data != "" then
+      Service.primary.update(:message => post_data)
     end
   end
-
   settings "VLC #nowplaying" do
     input 'VLC HTTP interface', :host
     input 'Basic auth password', :password
